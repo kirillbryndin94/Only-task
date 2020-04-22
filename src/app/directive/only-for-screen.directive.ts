317b94957ce5df10d1839fd62e113452166e65a8
kirillbryndin94 <kirillbryndin94@gmail.com>
2020-04-22 23:43:35 +0900

    Commit only For Screen

---
+++ src/app/directive/only-for-screen.directive.ts
@@ -0,0 +1,45 @@
+import { Directive, HostListener, ElementRef, OnInit, Input} from '@angular/core';
+interface IConfig {
+  mobile: number;
+  tablet: number;
+}
+
+@Directive({
+  selector: '[appOnlyForScreen]'
+})
+export class OnlyForScreenDirective {
+  scrWidth: number;
+  config: IConfig;
+
+
+  @Input('appOnlyForScreen') onlyForScreen: string;
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.scrWidth = window.innerWidth;
+    this.el.nativeElement.style.display = 'none';
+    if (this.scrWidth <= this.config.mobile && this.onlyForScreen === 'mobile') {
+      this.el.nativeElement.style.display = 'inline';
+    }
+
+    if (this.scrWidth > this.config.mobile && this.scrWidth <= this.config.tablet && this.onlyForScreen === 'tablet') {
+      this.el.nativeElement.style.display = 'inline';
+    }
+
+    if (this.scrWidth > this.config.tablet && this.onlyForScreen === 'desktop') {
+      this.el.nativeElement.style.display = 'inline';
+    }
+    return this.el.nativeElement;
+  }
+
+  constructor(private el: ElementRef) {
+    this.config = {mobile: 767, tablet: 992};
+    this.getScreenSize();
+  }
+
+  ngOnInit() {
+    this.getScreenSize();
+  }
+
+
+}
